ab6e5b19bbccb115bf3390fc4305a9dee36c6278
曹弘扬 <296786475@qq.com>
2020-03-17 16:52:09 +0800

    Home 添加PageStyle

--- Himalayan/Classes/Home/HYHomeViewController.swift
+++ Himalayan/Classes/Home/HYHomeViewController.swift
@@ -7,13 +7,32 @@
 //
 
 import UIKit
+import DNSPageView
 
 class HYHomeViewController: HYBaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 创建DNSPageStyle, 设置样式
+        let style = DNSPageView.PageStyle()
+        style.isTitleViewScrollEnabled = false
+        style.isTitleScaleEnabled = true
+        style.isShowBottomLine = true
+        style.titleSelectedColor = UIColor.black
+        style.titleColor = UIColor.gray
+        style.bottomLineColor = DominantColor
+        style.bottomLineHeight = 2
+        
+        let titles = ["推荐", "分类", "VIP", "直播", "广播"]
+        let viewControllers: [HYBaseViewController] = [HYHomeRecommendController(), HYHomeClassifyController(), HYHomeVIPController(), HYHomeLiveController(), HYHomeBroadcastController()]
+        for vc in viewControllers {
+            self.addChild(vc)
+        }
+        
+        let pageView = DNSPageView.PageView(frame: CGRect(x: 0, y: navigationBarHeight, width: ScreenWidth, height: ScreenHeight-navigationBarHeight-44), style: style, titles: titles, childViewControllers: viewControllers)
+        view.addSubview(pageView)
+        
     }
     
 
---
+++ Himalayan/Classes/Home/VIP/HYHomeVIPController.swift
@@ -0,0 +1,30 @@
+//
+//  HYHomeVIPController.swift
+//  Himalayan
+//
+//  Created by Adonis_HongYang on 2020/3/17.
+//  Copyright © 2020 Adonis_HongYang. All rights reserved.
+//
+
+import UIKit
+
+class HYHomeVIPController: HYBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
